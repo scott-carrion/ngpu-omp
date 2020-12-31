0da225069235d71d0f239c91cb39a12f26f1c1b7
Scott Carrion <scott.carrion@icloud.com>
2020-12-30 18:47:06 -0600

    Fixed some bugs. Output still incorrect, but outputting "ballpark" values

--- GridOps_tc.h
+++ GridOps_tc.h
@@ -48,7 +48,7 @@ double bilinearInterpolate ( double i, double j,
 
 template <typename T>
 double bilinearInterpolate_tc ( double i, double j,
-    GridCell<T>* v )
+    GridCell<T>* v, bool debug_flag )
 {
     double xmin = v[0].j + 0.5;
     double xrng = v[3].j - v[0].j;
@@ -64,6 +64,8 @@ double bilinearInterpolate_tc ( double i, double j,
     
     double c0 = 1.0 - y;
     double c1 = y;
+
+    if (debug_flag) { printf("v[0].value == %f; v[1].value == %f; v[2].value == %f; v[3].value == %f\nv[0].i == %d; v[1].i == %d; v[2].i == %d; v[3].i == %d\nv[0].j == %d; v[1].j == %d; v[2].j == %d; v[3].j == %d\n", v[0].value, v[1].value, v[2].value, v[3].value, v[0].i, v[1].i, v[2].i, v[3].i, v[0].j, v[1].j, v[2].j, v[3].j); }
     
     return B00 * a0 * c0
         + B10 * a1 * c0
@@ -105,15 +107,23 @@ std::vector<GridCell<T> > getNearest4 ( const Grid_tc<T> & g, double i0,
 // The below function is a version of the above that does NOT make use of C++ STL
 // so that it is compatible with NVIDIA hardware that doesn't support linking against the C++ STL
 template <typename T>
-void getNearest4_tc(const Grid_tc<T> & g, double i0, double j0, GridCell<T>* v)
-{
+void getNearest4_tc(const Grid_tc<T> & g, double i0, double j0, GridCell<T>* v, int nrows, int ncols, bool debug_flag) {
+
+    // FIXME: IN THIS FUNCTION, g.nrows() AND g.ncols() RETURN 0 EVEN WHEN THEY SHOULDN'T. WHY IS THIS?
+    //if (debug_flag) {
+    //	printf("g.nrows() is %d and g.ncols() is %d\n", g.nrows(), g.ncols());
+    //}
+
+
     // get upper-left cell /*
     int i = floor(i0 - 0.5);
     int j = floor(j0 - 0.5);
     if ( i < 0 ) i = 0;
     if ( j < 0 ) j = 0;
-    if ( i + 1 >= g.nrows() ) i = g.nrows() - 2;
-    if ( j + 1 >= g.ncols() ) j = g.ncols() - 2;
+    //if ( i + 1 >= g.nrows() ) i = g.nrows() - 2;
+    //if ( j + 1 >= g.ncols() ) j = g.ncols() - 2;
+    if ( i + 1 >= nrows ) i = nrows - 2;
+    if ( j + 1 >= ncols ) j = ncols - 2;
     
     // add points to vector
     //GridCell<T>* v = (GridCell<T>*)(std::malloc(sizeof(GridCell<T>) * 4));  // XXX instead of a std::vector, use std::malloc() and native array!
@@ -123,6 +133,8 @@ void getNearest4_tc(const Grid_tc<T> & g, double i0, double j0, GridCell<T>* v)
     v[1] = GridCell<T>(i,   j+1, g.getValue(i,   j+1));
     v[2] = GridCell<T>(i+1, j,   g.getValue(i+1, j));
     v[3] = GridCell<T>(i+1, j+1, g.getValue(i+1, j+1));
+    
+    if (debug_flag) { printf("v[0].value == %f; v[1].value == %f; v[2].value == %f; v[3].value == %f\nv[0].i == %d; v[1].i == %d; v[2].i == %d; v[3].i == %d\nv[0].j == %d; v[1].j == %d; v[2].j == %d; v[3].j == %d\n", v[0].value, v[1].value, v[2].value, v[3].value, v[0].i, v[1].i, v[2].i, v[3].i, v[0].j, v[1].j, v[2].j, v[3].j); }
     
     //return v;
 
@@ -201,17 +213,21 @@ Grid_tc<T> skyview ( const Grid_tc<T> & dem, int daz, double r )
     
     int count = 0;
 
+    // hard-coded values from dem here
+    int nrows = dem.nrows(); int ncols = dem.ncols();
+    double dem_dx = dem.dx(); T dem_no_data = dem.noData();
 
     // (old) CPU: #pragma omp parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, cells, count, angle, z) shared(interval, dmax, deg2rad, out) collapse(2)
     // GPU: #pragma omp target teams distribute parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, count, angle, z) shared(dem, interval, dmax, deg2rad, out) map(tofrom: dem.data[0:dem.get_volume()]) collapse(2) 
     // (new) CPU #pragma omp parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, count, angle, z) shared(interval, dmax, deg2rad, out) collapse(2) 
-    #pragma omp target teams distribute parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, count, angle, z) shared(dem, interval, dmax, deg2rad, out) map(tofrom: dem.data[0:dem.get_volume()]) collapse(2)
-    for ( i = 0; i < dem.nrows(); ++i ) {
-        for ( j = 0; j < dem.ncols(); ++j ) {
+#pragma omp target teams distribute parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, count, angle, z) shared(dem, interval, dmax, deg2rad, out, nrows, ncols) map(tofrom: out.data[0:out.get_volume()], dem.data[0:dem.get_volume()]) collapse(2)
+    //#pragma omp parallel for private(flag, i, ii, j, jj, k, kk, a, d, sinAz, cosAz, Amax, sum, count, angle, z) shared(interval, dmax, deg2rad, out) collapse(2)
+    for ( i = 0; i < nrows; ++i ) {
+        for ( j = 0; j < ncols; ++j ) { 
 	    GridCell<T>* cells = (GridCell<T>*)(std::malloc(sizeof(GridCell<T>) * 4));  // XXX declaring cells here, since it's used internally only anyway
             //std::cout << "\rskyview " << (static_cast<double>(count++) / dem.size()) << "%        ";
             // Using printf() instead
-	    printf("\rskyview %f%%        \n", (static_cast<double>(count++) / dem.size()));
+	    //printf("\rskyview %f%%        \n", (static_cast<double>(count++) / dem.size()));
         
             k = dem.getIndex(i, j);
         
@@ -224,24 +240,25 @@ Grid_tc<T> skyview ( const Grid_tc<T> & dem, int daz, double r )
                 
                 // find maximum angle from horizontal
                 Amax = 0.0;
-                
 		for ( d = 1; d <= dmax; ++d ) { 
                     // get coordinates at point
                     ii = (i+0.5) - d * cosAz;
                     jj = (j+0.5) + d * sinAz;
                     
 
-                    if ( ii < 0 || ii >= dem.nrows()
-                            || jj < 0 || jj >= dem.ncols() )
-                        break;
+                    if ( ii < 0 || ii >= nrows
+                            || jj < 0 || jj >= ncols )
+                        { break; }
+
                     // interpolate value at point
                     flag = false;
-                    getNearest4_tc(dem, ii, jj, cells);
+                    if (d < 2) { getNearest4_tc(dem, ii, jj, cells, nrows, ncols, true); }
+		    else { getNearest4_tc(dem, ii, jj, cells, nrows, ncols, false); }
 
 		// In getNearest4_tc, cells is set to an array of size 4. It is always size 4, so use that instead of
 		// what would normally be cells.size()
 		for ( kk = 0; !flag && kk < 4; ++kk ){
-                        if ( cells[kk].value == dem.noData() ) {  // XXX THE LINE CAUSING THE ISSUE???
+                        if ( cells[kk].value == dem_no_data ) {
                             flag = true;
 			}
                     } 
@@ -250,22 +267,26 @@ Grid_tc<T> skyview ( const Grid_tc<T> & dem, int daz, double r )
                        continue;
                     }
 
-                    z = bilinearInterpolate_tc(ii, jj, cells);
+                    if (d < 2) { z = bilinearInterpolate_tc(ii, jj, cells, false); }  // FIXME: GPU VERSION SETS z TO 0 EVERY TIME
+		    else { z = bilinearInterpolate_tc(ii, jj, cells, false); }
+
+		    //if (d < 2) { printf("z value is %f\n", z); }
 
                     // track maximum angle from horizontal
-                    angle = atan((z - dem[k]) / (d * dem.dx()));
-                    if ( angle > Amax ) Amax = angle;
+                    angle = atan((z - dem[k]) / (d * dem_dx));
+		    //if (d < 2) { printf("In kk loop, angle is: %f\n", angle); }
+                    if ( angle > Amax ) { Amax = angle; /*printf("New Amax is %f\n", Amax);*/ }
                 }  
-                
-                Amax = cos(Amax);
-                sum += Amax * Amax; 
-            }
-            out[k] = sum * interval;  // XXX Causing an issue as well!!!!
 
+                Amax = cos(Amax);
+                sum += Amax * Amax;
+            }
+	    //printf("Assigning out[%d] == %f * %f == %f\n", k, sum, interval, sum * interval);
+            out[k] = sum * interval;
         }
     }
     // std::cout << "\rskyview " << (static_cast<double>(count++) / dem.size()) << "%\n";
-    printf("\rskyview %f%%\n", (static_cast<double>(count++) / dem.size()));
+    // printf("\rskyview %f%%\n", (static_cast<double>(count++) / dem.size()));
  
     double timer_end = omp_get_wtime();
     std::cout << "Skyview took this many seconds: " << timer_end - timer_start << std::endl;
